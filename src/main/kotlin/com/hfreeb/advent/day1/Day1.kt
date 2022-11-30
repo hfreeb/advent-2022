7b06340a0cb2d1c2ac51b3362842272571b031cf
Harry Freeborough <harry@hfreeb.com>
2022-11-30 22:00:34 +0000

    Initial commit

---
+++ src/main/kotlin/com/hfreeb/advent/day1/Day1.kt
@@ -0,0 +1,19 @@
+package com.hfreeb.advent.day1
+
+val input = ClassLoader.getSystemResourceAsStream("day1.txt")!!
+    .bufferedReader()
+    .readLines()
+    .map { it.toInt() }
+
+fun part1(input: List<Int>): Int {
+    return 0
+}
+
+fun part2(input: List<Int>): Int {
+    return 0
+}
+
+fun main() {
+    println("part 1 = ${part1(input)}")
+    println("part 2 = ${part2(input)}")
+}
