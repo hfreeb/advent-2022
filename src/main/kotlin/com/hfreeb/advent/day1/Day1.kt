f74068a3ced52d47331977d20192ec90c7774ba0
Harry Freeborough <harry@hfreeb.com>
2022-12-01 06:48:58 +0000

    Add day 1 solution

--- src/main/kotlin/com/hfreeb/advent/day1/Day1.kt
+++ src/main/kotlin/com/hfreeb/advent/day1/Day1.kt
@@ -1,16 +1,18 @@
 package com.hfreeb.advent.day1
 
-val input = ClassLoader.getSystemResourceAsStream("day1.txt")!!
-    .bufferedReader()
-    .readLines()
-    .map { it.toInt() }
+val input = ClassLoader.getSystemResource("day1.txt")
+    .readText()
+    .split("\n\n")
+    .map { it.trim().lines().map(String::toInt) }
 
-fun part1(input: List<Int>): Int {
-    return 0
+fun part1(input: List<List<Int>>): Int {
+    return input.maxOf { it.sum() }
 }
 
-fun part2(input: List<Int>): Int {
-    return 0
+fun part2(input: List<List<Int>>): Int {
+    return input.map { it.sum() }
+        .sorted().takeLast(3)
+        .sum()
 }
 
 fun main() {
